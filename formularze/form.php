aa0a2f82640c3f396cc8e515c2795515fa0abba9
kubamarszalkiewicz <kuba_marszalkiewicz@ekos.edu.pl>
2014-11-18 17:37:20 +0100

    Update form.php

--- formularze/form.php
+++ formularze/form.php
@@ -1,5 +1,5 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 <?php
 print $_GET['imie'].' '.$_GET['nazwisko']."<br>"
-.'Urodzony/a'.$_GET['dd'].'.'.$_GET['mm'].'.'.$_GET['rrrr'].'.'."<br>"
+.'Urodzony/a '.$_GET['dd'].'.'.$_GET['mm'].'.'.$_GET['rrrr'].'.'."<br>"
 .'To ty?';
