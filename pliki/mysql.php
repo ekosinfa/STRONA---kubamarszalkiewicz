18862ae406cee4d2f0dc0b9d3c4ce82838df4e8d
kubamarszalkiewicz <kuba_marszalkiewicz@ekos.edu.pl>
2014-12-02 17:40:46 +0100

    Update mysql.php

--- pliki/mysql.php
+++ pliki/mysql.php
@@ -3,4 +3,4 @@ $connect=mysqli_connect("localhost","kuba","kubaxsw21qaz","ekosinfa_kuba");
 $result=mysqli_query($connect,'SELECT * FROM esport');
 $rows=mysqli_fetch_array($result);
 foreach($rows as $row){
-var_dump($rows);}
+echo $row;}
