72defae2db576eb441f09b10d04729598cd2205f
kubamarszalkiewicz <kuba_marszalkiewicz@ekos.edu.pl>
2014-12-02 18:01:38 +0100

    Update mysql.php

--- pliki/mysql.php
+++ pliki/mysql.php
@@ -1,10 +1,19 @@
+<!DOCTYPE html>
+<html lang="pl_PL"> 
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>E-sport</title>
+</head>
+<body>
+<table>
 <?php
 $connect=mysqli_connect("localhost","kuba","kubaxsw21qaz","ekosinfa_kuba");
 $result=mysqli_query($connect,'SELECT * FROM esport');
 while($rows=mysqli_fetch_array($result)){
-echo $rows['imie'];
-echo $rows['nick'];
-echo $rows['nazwisko'];
-echo $rows['wiek'];
-echo $rows['druzyna'];
+echo '<tr>'.'<td>'.$rows['imie'].'</td>';
+echo '<td>'.$rows['nazwisko'].'</td>';
+echo '<td>'.$rows['nick'].'</td>';
+echo '<td>'.$rows['wiek'].'</td>';
+echo '<td>'.$rows['druzyna'].'</td>';
 }
+</body>
