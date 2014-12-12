72e5106f26af292eb9bd148d37ed8cf027f32f22
kubamarszalkiewicz <kuba_marszalkiewicz@ekos.edu.pl>
2014-12-12 17:53:10 +0100

    Update and rename cwic.html to cwic.php

---
+++ pliki/cwic.php
@@ -0,0 +1,9 @@
+<?php
+$licznik = 6;
+$liczba = 3875;
+print 'Suma to: '.$licznik*$liczba;
+<br><br>
+echo 'Napisz kod, ktorego wynikiem bedzie to co wyzej';
+<br>
+print 'Potrzebne wam będą dwie zmienne: $licznik = 6 i $liczba = 3875.';
+?>
