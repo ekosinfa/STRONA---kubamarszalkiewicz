5324b74b8327a77ff3b92a8f69d49e443f7ca5e3
kubamarszalkiewicz <kuba_marszalkiewicz@ekos.edu.pl>
2014-12-12 17:32:34 +0100

    Update cwic.php

--- pliki/cwic.php
+++ pliki/cwic.php
@@ -1,8 +1,9 @@
 <?php
 $licznik = 6;
 $liczba = 3875;
-print 'Suma to: '.$licznik*$liczba."\r\n"."\r\n";
+print 'Suma to: '.$licznik*$liczba;
+<br><br>
 echo 'Napisz kod, którego wynikiem bedzie to co wyżej';
-echo "\r\n";
+<br>
 print 'Potrzebne wam będą dwie zmienne: $licznik = 6 i $liczba = 3875.';
 ?>
