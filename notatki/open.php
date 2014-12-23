65e9cfbe7b5c6c874b4da178434e7f0ac33e5510
kubamarszalkiewicz <kuba_marszalkiewicz@ekos.edu.pl>
2014-12-23 16:41:43 +0100

    Create open.php

---
+++ notatki/open.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pl_PL"> 
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>Notatki-otwórz</title>
+</head>
+<body>
+<?php
+$nazwa = $_GET['nazwa'];
+$connect=mysqli_connect("localhost","kuba","kubaxsw21qaz","ekosinfa_kuba");
+$result=mysqli_query($connect,'SELECT $nazwa FROM notes');
+<table>
+while($rows=mysqli_fetch_array($result)){
+echo '<tr>'.'<td>'.$rows['title'].'</td>';
+echo '<td>'.$rows['notes'].'</td>';
+}
+?>
+</table>
+</body>
+</html>
