040586d4fc85261a1ea975c5e06e83f4bc9e1e1c
kubamarszalkiewicz <kuba_marszalkiewicz@ekos.edu.pl>
2014-12-23 16:49:22 +0100

    Update open.php

--- notatki/open.php
+++ notatki/open.php
@@ -6,12 +6,13 @@
 </head>
 <body>
 <?php
-$nazwa = $_GET['nazwa'];
+$nazwa = $_GET['title'];
 $connect=mysqli_connect("localhost","kuba","kubaxsw21qaz","ekosinfa_kuba");
 $result=mysqli_query($connect,'SELECT $nazwa FROM notes');
-$rows=mysqli_fetch_array($result);
+while($rows=mysqli_fetch_array($result)){
 echo $rows['title'];
 echo $rows['notes'];
+};
 ?>
 </body>
 </html>
