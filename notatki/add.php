3b1ede0a3ded6ac48039347122ff64196eff92e8
Marcin Ławniczak <marcin.safmb@wp.pl>
2015-01-23 17:02:09 +0100

    Update add.php

--- notatki/add.php
+++ notatki/add.php
@@ -12,11 +12,11 @@ if($nazwa and $tresc) {
     //or die('Nie mogę połączyć się z bazą danych'); 
      
     // dodajemy rekord do bazy 
-    $query = "INSERT INTO notes ( title,notes ) VALUES ($nazwa,$tresc)";
+    $query = "INSERT INTO notes ( title,notes ) VALUES ('$nazwa','$tresc')";
     var_dump($query);
     $ins = mysqli_query($query); 
     if($ins) echo "Rekord został dodany poprawnie"; 
-    else echo "Błąd nie udało się dodać nowego rekordu".mysqli_error($connection); 
+    else echo "Błąd nie udało się dodać nowego rekordu"; 
      
     mysqli_close($connection); 
 } 
